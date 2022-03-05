a2212cb36086ff0eaa683709c34034acbf1f5531
emmanuelouedraogo <emmanuelrhema.amjc@gmail.com>
2022-03-05 15:08:11 +0100

    first saving

---
+++ js/main.js
@@ -0,0 +1,14 @@
+alert("hello");
+let weightLabel = document.querySelector('label[for="weight"]');
+let weightInput = document.querySelector('#weight');
+let sampleText = document.querySelector('.sample');
+
+function update() {
+  weightLabel.textContent = `font-weight: ${weightInput.value};`;
+  sampleText.style.fontWeight = weightInput.value;
+}
+
+weightInput.addEventListener('input', update);
+
+update();
+
